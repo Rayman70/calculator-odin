239c741953dcebc6562ff96b78dbb3446c32fb9f
Ramon Morales <rayman_mora70@yahoo.com>
2023-05-05 22:08:08 -0500

    Make basic operator functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function calcAdd(num) {
+    return num += 0;
+}
+
+function calcSub(num) {
+    return num -= 0;
+}
+
+function calcMulti(num) {
+    return num *= 1;
+}
+ 
+function calcDiv(num) {
+    return num /= 1;
+ }
