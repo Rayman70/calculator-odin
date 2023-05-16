eea3a56455dc10861b79576afaac3758e8e6a1bb
Ramon Morales <rayman_mora70@yahoo.com>
2023-05-15 19:12:17 -0500

    Change logic and add operate

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-function calcAdd(num) {
-    return num += 0;
+
+function calcAdd(num1, num2) {
+    return num1 += num2;
 }
 
-function calcSub(num) {
-    return num -= 0;
+function calcSub(num1, num2) {
+    return num1 -= num2;
 }
 
-function calcMulti(num) {
-    return num *= 1;
+function calcMulti(num1, num2) {
+    return num1 *= num2;
 }
  
-function calcDiv(num) {
-    return num /= 1;
+function calcDiv(num1, num2) {
+    return num1 /= num2;
  }
+
+function operate(num1, num2, op) {
+    if(op === "+") {
+        return calcAdd(num1, num2);
+    } else if (op === "-") {
+        return calcSub(num1, num2);
+    } else if (op === "*") {
+        return calcMulti(num1, num2);
+    } else if (op === "/") {
+        return calcDiv(num1, num2);
+    }
+}
+
+
